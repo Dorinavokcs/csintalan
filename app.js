4ab08f911e59dc2605e765e605c585886158d433
l.cs.dorina <l.cs.dorina@gmail.com>
2024-04-24 20:18:02 +0200

    nehezites

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-function render(){
+function render() {
     const table = document.querySelector(".table");
     table.innerHTML = "";
-    for(let i = 0; i<9; i++){
-        table.innerHTML +=`
+    for (let i = 0; i < 9; i++) {
+        table.innerHTML += `
         <div class="box box${i}"><div>`
     }
 }
@@ -13,6 +13,19 @@ const down = document.querySelector(".down");
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 
-let sorszam=0;
-let kivalasztva="./kepek1"
-let lepesek=0;
+let sorszam = 0;
+let kivalasztva = "./kepek1"
+let lepesek = 0;
+$('.toggle').click(function() {
+    "use strict";
+    $('nav ul').slideToggle();
+});
+
+
+
+$(window).resize(function() {
+    "use strict";
+    if ($(window).width() > 780) {
+        $('nav ul').removeAttr('style');
+    }
+});

1214cacfa82609c5ef032c452ace8694df3ac735
l.cs.dorina <l.cs.dorina@gmail.com>
2024-04-24 13:06:04 +0200

    cicahaj

---
+++ app.js
@@ -0,0 +1,8 @@
+function render(){
+    const table = document.querySelector(".table");
+    table.innerHTML = "";
+    for(let i = 0; i<9; i++){
+        table.innerHTML +=`
+        <div class="box box${i}"><div>`
+    }
+}
